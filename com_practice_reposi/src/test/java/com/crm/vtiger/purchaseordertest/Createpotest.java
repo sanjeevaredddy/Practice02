c47fd0e0e097d5aa2ce263458c46cc111dffe546
admin <admin@sanjureddy>
2021-03-15 16:09:01 +0530

    Createpo

---
+++ com_practice_reposi/src/test/java/com/crm/vtiger/purchaseordertest/Createpotest.java
@@ -0,0 +1,11 @@
+package com.crm.vtiger.purchaseordertest;
+
+import org.testng.annotations.Test;
+
+public class Createpotest {
+   @Test
+   public void Createpotest()
+   {
+	   System.out.println("Create a po");
+   }
+}
