9085f5153b55905ff79c379fb8a243aa270f4891
admin <admin@sanjureddy>
2021-03-15 15:59:08 +0530

    Practice second time

---
+++ com_practice_reposi/src/main/java/practicerepository/com_practice_reposi/App.java
@@ -0,0 +1,13 @@
+package practicerepository.com_practice_reposi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
